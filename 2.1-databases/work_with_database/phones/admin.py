6b691de029cc0c536d0a09801ac3aeb4dcf87aa6
AlexBazovkin <alexbaz@vk.com>
2022-10-26 21:44:19 +0400

    1.2-requests-templates homework || Phones

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Phone
 # Register your models here.
+
+
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Phone, PhoneAdmin)
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=1)
+    image = models.CharField(max_length=100)
+    release_date = models.CharField(max_length=20)
+    lte_exists = models.BooleanField()
+
+
+def get_absolute_url(self):
+    return reverse('phone_detail', kwargs={'slug': self.slug})
