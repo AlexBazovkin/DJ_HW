b5564a537d38c07e982bc5974b3aa39bcb15bb69
AlexBazovkin <alexbaz@vk.com>
2022-11-17 16:04:54 +0400

    yes

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=15)
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
@@ -8,9 +17,22 @@ class Article(models.Model):
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
 
+    # tag = models.ManyToManyField(Tag, related_name='articles')
+
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    # таблица связка между статьей и тегом.
+    # Именно здесь должно быть свойство `is_main`
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tags')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='tags')
+
+
+
+
